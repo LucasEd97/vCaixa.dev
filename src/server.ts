efac5e98fc3af8ff4881644317f7fb65f97f9ef8
lucas ferreira <lucas.98713@gmail.com>
2020-08-23 21:00:10 -0300

    Ajustando porta para Heroku

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('SERVIDOR INICIADO NA PORTA 3333');
 });
